a420a6cf8c4620a2f4e6a7c09d57d3bc11bf7dc3
harsh samoliya <65808775+harshsamoliya@users.noreply.github.com>
2020-12-17 22:49:27 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,5 +1,9 @@
 // Two Sum (a+b=target)
+// Time Complexity o(n)
+// Space Complexity o(n)
 
+
+// meathod 1 
 import java.util.*;
 
 class Program {
@@ -15,5 +19,28 @@ class Program {
 			}
 		}
 		return new int[0];
+  }
+}
+// meathod 2 Two pointers
+// Time Complexity O(nlog(n))
+// Space Complexity O(1)
+import java.util.*;
+
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+		Arrays.sort(array);
+		int i = 0;
+		int j = array.length-1;
+		while(i<j){
+			int target =array[i] + array[j];
+			if(target==targetSum){
+				return new int[]{array[i],array[j]};
+			}else if(target<targetSum){
+				i++;
+			}else{
+				j--;
+			}
+		}
+    return new int[0];
   }
 }
