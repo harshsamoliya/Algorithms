f69984853675654b784aef2d1f251fc010a38cfd
harsh samoliya <65808775+harshsamoliya@users.noreply.github.com>
2020-12-18 21:52:42 +0530

    Add files via upload

---
+++ threeSum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Program {
+  public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
+    Arrays.sort(array);
+		List<Integer[]> list = new ArrayList<>();
+		for(int i = 0 ;i<array.length;i++){
+			int current_no = i;
+			int left = i+1;
+			int right  = array.length-1;
+			while(left<right){
+				int current_sum = array[current_no] + array[left] + array[right];
+				if(current_sum==targetSum){
+					Integer[] lt = {array[current_no],array[left],array[right]};
+					list.add(lt);
+					left++;
+					right--;
+				}else if(current_sum<targetSum){
+					left++;
+				}else{
+					right--;
+				}
+			}
+		}
+		return list;
+	}
+}
