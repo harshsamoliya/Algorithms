678aeb14ecc47449870ce1bbeec80d2953771e88
harsh samoliya <65808775+harshsamoliya@users.noreply.github.com>
2020-12-23 23:59:10 +0530

    Add files via upload

---
+++ bubbleSort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Program {
+  public static int[] bubbleSort(int[] array) {
+		if(array.length==0){
+			return new int[]{};
+		}
+		for(int i = 0 ;i<array.length;i++){
+			for(int j = 1;j<array.length;j++){
+				if(array[j]<array[j-1]){
+					swap(array,j,j-1);
+				}
+			}
+		}
+		return array;
+  }
+	private static void swap(int[] array , int i , int j){
+		if(i==j){
+			return;
+		}
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+}
