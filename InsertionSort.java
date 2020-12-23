3624da4ea0f285abd49c660a5e26d0b9e0f01e39
harsh samoliya <65808775+harshsamoliya@users.noreply.github.com>
2020-12-24 00:01:53 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,5 +1,9 @@
 import java.util.*;
+// Best->O(n)
+// Avg -> O(n^2)
+// Worst -> O(n^2)
 
+// Space->O(1)
 class Program {
   public static int[] insertionSort(int[] array) {
 		for(int gap = array.length/2;gap>0;gap/=2){
