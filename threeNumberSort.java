07c231562443a03b253c180442efe82bdf9b2108
harsh samoliya <65808775+harshsamoliya@users.noreply.github.com>
2020-12-28 20:47:40 +0530

    Update threeNumberSort.java

--- threeNumberSort.java
+++ threeNumberSort.java
@@ -1,7 +1,12 @@
+
+// Time Complexity O(n)
+// Space Complexity O(1)
+
+
 import java.util.*;
 
 class Program {
-  public int[] threeNumberSort(int[] array, int[] order) {
+  	public int[] threeNumberSort(int[] array, int[] order) {
 		int f  = order[0];
 		int s  = order[1];
 		int t  = order[2];
@@ -19,18 +24,39 @@ class Program {
 				swap(array,second,third);
 				third--;
 					// no action on second value
-				}else{
-					second++;
-				}
+			}else{
+				second++;
+			}
 		}
 		return array;
 	}
-		private void swap(int[] array,int i,int j){
-			if(i==j){
-				return;
-			}
-			int temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
+	private void swap(int[] array,int i,int j){
+		if(i==j){
+			return;
 		}
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	private static int[] threeNumberSort2(int[] array,int[] order){
+        int f = order[0];
+        int t = order[2];
+        int firstIdx = 0;
+        for(int i = 0;i<array.length;i++){
+            if(array[i]==f){
+                swap(array,firstIdx,i);
+                firstIdx++;
+            }
+        }
+        int thirdIdx = array.length-1;
+        for(int i = array.length-1;i>=0;i--){
+            if(array[i]==t){
+                swap(array,thirdIdx,i);
+                thirdIdx--;
+            }
+        }
+
+        return array;
+    }
 }
+
