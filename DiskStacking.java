428f7652b7137a8f67a9ab4eabb5888b19d4042d
harsh samoliya <65808775+harshsamoliya@users.noreply.github.com>
2021-03-25 20:14:51 +0530

    Update DiskStacking.java

--- DiskStacking.java
+++ DiskStacking.java
@@ -1,16 +1,19 @@
+// Time complexity -> O(n*n)
+// space complexity -> O(n)
+// disk have dimension of width,depth,height
+
 import java.util.*;
 
 class Program {
-    public static List<Integer[]> diskStacking(List<Integer[]> disks) {
+    public static int  diskStacking(List<Integer[]> disks) {
         disks.sort((disks1,disks2) -> disks1[2].compareTo(disks2[2]));
+        
         int height[] = new int[disks.size()];
         for(int i = 0; i<disks.size();i++){
             height[i] = disks.get(i)[2];
         }
-        int sequence[] = new int[disks.size()];
-        Arrays.fill(sequence,Integer.MIN_VALUE);
         //
-        int maxHeighIndex = 0;
+        int max = 0;
         for(int i = 0; i<disks.size();i++){
             Integer[] currentDisk = disks.get(i);
             for(int j = 0;j<i;j++){
@@ -18,25 +21,14 @@ class Program {
                 if(check(otherDisk,currentDisk)){
                     if(height[i]<=height[j] + currentDisk[2]){
                         height[i] =height[j] + currentDisk[2];
-                        sequence[i] = j;
+                        max = Math.max(max,height[i]);
                     }
                 }
             }
-            if(height[i]>=height[maxHeighIndex]){
-                maxHeighIndex = i;
-            }
         }
-        return buildSequence(disks,sequence,maxHeighIndex);
+        return max;
     }
     public static Boolean check(Integer[] o,Integer[] c){
         return o[0]<c[0] && o[1] < c[1] && o[2] < c[2];
-    }
-    public static List<Integer[]> buildSequence(List<Integer[]> disk, int []sq,int maxheightIndex){
-        List<Integer[]> sequence = new ArrayList<>();
-        while(maxheightIndex!=Integer.MIN_VALUE){
-            sequence.add(0,disk.get(maxheightIndex));
-            maxheightIndex = sq[maxheightIndex];
-        }
-        return sequence;
     }
 }
